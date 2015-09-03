52f689d4506d2d889cb4b0a1a872a05a5df4b8f9
arman2152 <arman.yessymkanov@gmail.com>
2015-09-03 07:47:50 +0600

    Responsive navigation is added

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+console.log(height);
