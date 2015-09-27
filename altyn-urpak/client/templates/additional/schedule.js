cc7753c8acb9ab651bd83396ea8dfbe2660d2939
Abzal Serekov <abzal.serekov@gmail.com>
2015-09-28 00:05:52 +0600

    schedule js now works

--- altyn-urpak/client/templates/additional/schedule.js
+++ altyn-urpak/client/templates/additional/schedule.js
@@ -2,3 +2,13 @@ Template.schedule.rendered = function () {
 	$("body").addClass("custom-body").removeClass("blog-post-body");
 	$("footer").removeClass("footer-wrapper").removeClass("blog-post-footer").addClass("footer-not-main");
 }
+
+Template.schedule.events({
+	'click .js-schedule-nav-item': function (e) {
+		e.preventDefault();
+		var $this = $(e.currentTarget);
+		$this.addClass('active').siblings().removeClass('active');
+		$("."+$this.data("table")).addClass("active").siblings("table").removeClass("active");
+
+	}
+});
